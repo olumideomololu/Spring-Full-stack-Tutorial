84d516a84fb933a5ce1415f541bf011bebdb4287
olumideomololu <oaomololu@gmail.com>
2024-01-17 21:29:33 +0000

    frontend update

--- pubReg-FrontEnd/src/App.jsx
+++ pubReg-FrontEnd/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.css";
+import api from "./api/axiosConfig";
+import { useEffect, useState } from "react";
+import "./App.css";
+import PublisherCrud from "./components/PublisherCrud";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [publishers, setPublishers] = useState([]);
+
+  /* manage side effects */
+  useEffect(() => {
+    (async () => await load())();
+  }, []);
+
+  async function load() {
+    const result = await api.get("/all");
+    setPublishers(result.data);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1 className="text-center">List Of Publisher</h1>
+      <PublisherCrud load={load} publishers={publishers} />
+    </div>
+  );
 }
 
-export default App
+export default App;
